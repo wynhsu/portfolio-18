840a96a9894ab88f69dba2535e4f33f1b51820ff
wynhsu <wynhsu@uw.edu>
2018-02-02 01:13:18 -0800

    trying stuff

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,9 +3,20 @@ import Nav from '../components/Nav';
 import Card from '../components/Card';
 import constants from '../components/constants';
 import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
+import { ScrollToTopOnMount, SectionsContainer, Section, Header } from 'react-fullpage';
 
 export default class Home extends React.Component {
     render() {
+        let options = {
+            sectionClassName: 'section',
+            anchors: ['sectionOne', 'sectionTwo', 'sectionThree'],
+            scrollBar: false,
+            navigation: true,
+            verticalAlign: false,
+            sectionPaddingTop: '50px',
+            sectionPaddingBottom: '50px',
+            arrowNavigation: true
+        };
         return (
             <div>
                 <div className='sticky-top'>
@@ -18,22 +29,32 @@ export default class Home extends React.Component {
                         <Nav title='Contact' link={constants.routes.contact} />
                     </ul>
                 </div>
-                {/* <div className='jumbotron jumbotron-fluid'>
-                    <h1>Lorem ipsum</h1>
-                    <p>Lorem ipsum</p>
-                </div> */}
 
                 <ParallaxProvider>
                     <Parallax slowerScrollRate>
-                    
-                        <img src="./img/test.jpg" alt="polynesia" />
+                        <h1 className='title'>Hello World</h1>
+                        <p className='title'>Portfolio created by Wynston Hsu</p>
+                        <div className='cover'></div>
+                        <img className='splash' src="./img/test.jpg" alt="polynesia" />
                     </Parallax>
                 </ParallaxProvider>
+
+                {/* <ScrollToTopOnMount />
+                <SectionsContainer {...options}>
+                    <Section><img src={constants.images.diets} alt="diets"/></Section>
+                    <Section><img src={constants.images.breez} alt="breez"/></Section>
+                    <Section>Page 3</Section>
+                </SectionsContainer> */}
 
                 <div className='whitespace'>
                     <div className='container'>
                         <div className='row'>
-                            <Card src={constants.images.breez}
+                            <div className='col-lg-4 rounded'>
+                                <h4 className='card-title p-0'>text</h4>
+                                <p className='card-text'>text</p>
+                                <img className='rounded' src={constants.images.breez} alt="breez" />
+                            </div>
+                            {/* <Card src={constants.images.breez}
                                 alt='home screen'
                                 title='Design'
                                 text='Designs that are natural to the human intuition and convenient for everyday life.'
@@ -47,7 +68,7 @@ export default class Home extends React.Component {
                                 alt='game'
                                 title='Coding'
                                 text='Coming soon!'
-                                link={constants.routes.coding} />
+                                link={constants.routes.coding} /> */}
                         </div>
                     </div>
                 </div>
